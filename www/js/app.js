0c007dd4a93384cdb8222e10a9fe3d12c13446c4
Thibault <souef.thibault@gmail.com>
2013-07-19 17:30:34 +0200

    mise en place js

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,52 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+(function() {
+    "use strict";
+
+    var app = window.app = {
+        userdata: {},
+        module: {
+            _INSTANCE: function() {
+
+            }
+        },
+        init: function() {
+//            alert("init");
+            app.ready();
+        },
+        ready: function() {
+//            alert("ok");
+
+            app.setUserData("data-1", 1234);
+            for (var attr in app.module) {
+                if (attr !== "_INSTANCE") {
+                    app.module._INSTANCE[attr] = app.module[attr]();
+                }
+            }
+            
+        },
+        getUserData: function(name) {
+            if (app.userdata.name) {
+                return app.userdata.name;
+            } else {
+                return null;
+            }
+        },
+        setUserData: function(name, value) {
+            if (name && value) {
+                app.userdata.name = value;
+            }
+        }
+    };
+    $(document).ready(app.init);
+
+})();
 
 
+(function(app) {
+
+    app.slideshow = function() {
+//        alert("slideshow");
+        console.log(app);
+        console.log(app.getUserData("data-1"));
+    };
+
+})(app.module);
